13559bf5464be6a2872dfdc8bca13fa0fa9b1b35
MianyangIsland <2271967580@qq.com>
2023-03-27 00:14:36 +0800

    3/27 新增困难学生页面

---
+++ manage/src/pages/Sim/Dsi/api/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+// 新增平困生信息
+export const submitForm = ( params ) => {
+    return axios.post('/api/difficultStudent/add',params,{
+        headers: {
+            'token': localStorage.getItem('token'),
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
+// 查看贫困生基本信息列表
+export const getList = ( params ) => {
+    return axios.get('/api/difficultStudent/list',{
+        headers: { 'token': localStorage.getItem('token') },
+        params,
+    })
+}
+
+// 删除贫困生信息
+export const deleteStu = ( params ) => {
+    return axios.post('/api/difficultStudent/delete',params,{
+        headers: {
+            'token': localStorage.getItem('token'),
+            'Content-Type': 'application/json'
+        }
+    })
+}
