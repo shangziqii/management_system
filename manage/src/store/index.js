d0ecd76da4596c4352a0c324d1c8d9aa43b02971
szq <3156275966@qq.com>
2023-04-12 20:28:55 +0800

    上传
    刷新丢失问题解决

--- manage/src/store/index.js
+++ manage/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
 import detail from './detail'
 import showNav from './showNav'
 
@@ -10,5 +12,11 @@ export default new Vuex.Store({
     modules: {
         detail,
         showNav
-    }
+    },
+    plugins: [
+        createPersistedState({
+            key: 'my-app',
+            storage: window.localStorage
+        })
+    ]
 })
